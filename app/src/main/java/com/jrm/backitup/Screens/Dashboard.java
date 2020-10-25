740f91f3d3b08863f94330e805288d4bc313070d
codeosdev211 <codeos.dev211@gmail.com>
2020-10-25 12:35:21 +0530

    commit 24Oct20 1235

--- app/src/main/java/com/jrm/backitup/Screens/Dashboard.java
+++ app/src/main/java/com/jrm/backitup/Screens/Dashboard.java
@@ -2,15 +2,39 @@ package com.jrm.backitup.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.jrm.backitup.Local.AppPref;
 import com.jrm.backitup.R;
 
+import org.json.JSONObject;
+
 public class Dashboard extends AppCompatActivity {
+
+    JSONObject currentUser = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+        try {
+            currentUser = new JSONObject(new AppPref().localData(getApplicationContext(), 'G', "BU", ""));
+        }catch(Exception error) {
+            toast("Could not load page", 1);
+        }
     }
+
+    //defaults
+    private void redirect(Class<?> destination) {
+        Intent goTo = new Intent(getApplicationContext(), destination);
+        startActivity(goTo);
+        finish();
+    }
+
+    private void toast(String message, int duration) {
+        Toast.makeText(getApplicationContext(), message, duration).show();
+    }
+
 }
