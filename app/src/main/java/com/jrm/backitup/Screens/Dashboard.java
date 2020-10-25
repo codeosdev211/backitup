99c2c3c2416b706b69ff9a2517d65f776a4fc892
codeosdev211 <codeos.dev211@gmail.com>
2020-10-25 16:59:38 +0530

    commit 24Oct20 1659

--- app/src/main/java/com/jrm/backitup/Screens/Dashboard.java
+++ app/src/main/java/com/jrm/backitup/Screens/Dashboard.java
@@ -1,9 +1,14 @@
 package com.jrm.backitup.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.jrm.backitup.Local.AppPref;
@@ -14,6 +19,8 @@ import org.json.JSONObject;
 public class Dashboard extends AppCompatActivity {
 
     JSONObject currentUser = null;
+    private final int requestCode = 911;
+    private final int fileCode = 211;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +43,53 @@ public class Dashboard extends AppCompatActivity {
     private void toast(String message, int duration) {
         Toast.makeText(getApplicationContext(), message, duration).show();
     }
+
+    private void checkPermission(String permissionName) {
+        if(ContextCompat.checkSelfPermission(
+            getApplicationContext(), permissionName) == PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(
+                    Dashboard.this,
+                    new String[] { permissionName },
+                    requestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] results) {
+        super.onRequestPermissionsResult(requestCode, permissions, results);
+
+        if(requestCode == this.requestCode && results.length > 0
+            && results[0] == PackageManager.PERMISSION_GRANTED) {
+
+            toast("Permissons Granted", 0);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int fileCode, int resultCode, Intent data) {
+        super.onActivityResult(fileCode, resultCode, data);
+
+        if (fileCode == this.fileCode && resultCode == -1) {
+            Uri fileUri = data.getData();
+            String filePath = fileUri.getPath();
+            toast(filePath, 1);
+            // continue here later ... task (convert file to array of bytes
+        }
+    }
+
+    // onclick
+    public void chooseFiles(View view) {
+        Intent file = new Intent(Intent.ACTION_GET_CONTENT);
+        file.setType("*/*");
+        file = Intent.createChooser(file, "Select files...");
+        startActivityForResult(file, fileCode);
+    }
+
+
+
+
+
 
 }
