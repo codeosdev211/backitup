774b145a882f74a743b51a023a8938126bd7aef5
codeosdev211 <codeos.dev211@gmail.com>
2020-11-01 00:37:01 +0530

    commit 01Nov20 0031

--- app/src/main/java/com/jrm/backitup/Screens/Dashboard.java
+++ app/src/main/java/com/jrm/backitup/Screens/Dashboard.java
@@ -1,20 +1,36 @@
 package com.jrm.backitup.Screens;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.jrm.backitup.Local.AppPref;
 import com.jrm.backitup.R;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 
 public class Dashboard extends AppCompatActivity {
 
@@ -28,6 +44,7 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.dashboard);
         try {
             currentUser = new JSONObject(new AppPref().localData(getApplicationContext(), 'G', "BU", ""));
+            checkPermission();
         }catch(Exception error) {
             toast("Could not load page", 1);
         }
@@ -44,13 +61,18 @@ public class Dashboard extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), message, duration).show();
     }
 
-    private void checkPermission(String permissionName) {
+    private void checkPermission() {
         if(ContextCompat.checkSelfPermission(
-            getApplicationContext(), permissionName) == PackageManager.PERMISSION_DENIED) {
+            getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+        && ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ) {
 
             ActivityCompat.requestPermissions(
                     Dashboard.this,
-                    new String[] { permissionName },
+                    new String[] {
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },
                     requestCode);
         }
     }
@@ -61,8 +83,8 @@ public class Dashboard extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, results);
 
         if(requestCode == this.requestCode && results.length > 0
-            && results[0] == PackageManager.PERMISSION_GRANTED) {
-
+            && results[0] == PackageManager.PERMISSION_GRANTED
+            && results[1] == PackageManager.PERMISSION_GRANTED) {
             toast("Permissons Granted", 0);
         }
     }
@@ -70,22 +92,106 @@ public class Dashboard extends AppCompatActivity {
     @Override
     public void onActivityResult(int fileCode, int resultCode, Intent data) {
         super.onActivityResult(fileCode, resultCode, data);
-
+        try {
         if (fileCode == this.fileCode && resultCode == -1) {
-            Uri fileUri = data.getData();
-            String filePath = fileUri.getPath();
-            toast(filePath, 1);
-            // continue here later ... task (convert file to array of bytes
+            // if multiple select then into if else single selection
+            if(data.getClipData() != null) {
+                for(int each = 0; each < data.getClipData().getItemCount(); each++) {
+//                    appendFilesList(data.getClipData().getItemAt(each).getUri().getPath());
+                }
+            }else{
+                appendFilesList(data.getData());
+            }
+        }
+        }catch(Exception error) {
+            toast(error.getMessage(), 1);
         }
     }
 
-    // onclick
-    public void chooseFiles(View view) {
-        Intent file = new Intent(Intent.ACTION_GET_CONTENT);
-        file.setType("*/*");
-        file = Intent.createChooser(file, "Select files...");
-        startActivityForResult(file, fileCode);
+    private void appendFilesList(Uri fileUri) {
+        try {
+            // converting files to byte array
+            String fileName = getFileName(fileUri);
+            InputStream stream = getContentResolver().openInputStream(fileUri);
+            byte[] data = getBytes(stream);
+            // download files code below
+            String destPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/" + fileName;
+            FileOutputStream outStream = new FileOutputStream(destPath);
+            outStream.write(data);
+            outStream.close();
+
+        }catch(Exception error) {
+            toast("Error" + error.getMessage(), 1);
+        }
     }
+
+    private String getFileName(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        if(cursor.getCount() <= 0) {
+            toast("cant obtain file name", 1);
+            cursor.close();
+        }
+        cursor.moveToFirst();
+
+        String fileName = cursor.getString(cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME));
+
+        cursor.close();
+        return fileName;
+    }
+
+    private byte[] getBytes(InputStream stream) throws Exception {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while((len = stream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, len);
+        }
+        return outStream.toByteArray();
+    }
+
+    // onclick
+    private void chooseFiles() {
+            Intent file = new Intent(Intent.ACTION_GET_CONTENT);
+            file.setType("*/*");
+            file.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            file = Intent.createChooser(file, "Select files...");
+            startActivityForResult(file, fileCode);
+    }
+
+    private void uploadFiles() { }
+
+    public void selectFiles(View view) {
+        try {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            ViewGroup parent = findViewById(android.R.id.content);
+            View layout = LayoutInflater.from(view.getContext()).inflate(R.layout.select_files, parent, false);
+            builder.setView(layout);
+            AlertDialog dialog = builder.create();
+            dialog.setCancelable(true);
+            dialog.show();
+
+            // onclicks for dialog
+            Button selectBtn = layout.findViewById(R.id.selectBtn);
+            Button uploadBtn = layout.findViewById(R.id.uploadBtn);
+            selectBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    chooseFiles();
+                }
+            });
+            uploadBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    uploadFiles();
+                }
+            });
+        }catch(Exception error) {
+            toast("Could not load dialogbox", 0);
+        }
+    }
+
 
 
 
