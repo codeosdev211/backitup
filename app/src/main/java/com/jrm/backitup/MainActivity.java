6f9c6c818909644115bf47966dff9d2d2c03ecce
codeosdev211 <codeos.dev211@gmail.com>
2020-11-10 10:17:28 +0530

    commit 10Nov20 1017

--- app/src/main/java/com/jrm/backitup/MainActivity.java
+++ app/src/main/java/com/jrm/backitup/MainActivity.java
@@ -1,9 +1,14 @@
 package com.jrm.backitup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.jrm.backitup.Local.AppPref;
 import com.jrm.backitup.Screens.Dashboard;
@@ -11,15 +16,34 @@ import com.jrm.backitup.Screens.Login;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayoutCompat hiddenPanel;
+    boolean isOpen = false;
+    Animation bottomDown, bottomUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(new AppPref().localData(getApplicationContext(), 'G', "isLoggedIn", "").equals("Yes")) {
-            redirect(Dashboard.class);
+//        if(new AppPref().localData(getApplicationContext(), 'G', "isLoggedIn", "").equals("Yes")) {
+//            redirect(Dashboard.class);
+//        }else{
+//            redirect(Login.class);
+//        }
+        hiddenPanel = findViewById(R.id.hiddenPanel);
+        bottomDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_down);
+        bottomUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_up);
+    }
+
+    public void animate(View view) {
+        if(isOpen) {
+            hiddenPanel.startAnimation(bottomDown);
+            hiddenPanel.setVisibility(View.GONE);
+            isOpen = false;
         }else{
-            redirect(Login.class);
+            hiddenPanel.startAnimation(bottomUp);
+            hiddenPanel.setVisibility(View.VISIBLE);
+            isOpen = true;
         }
     }
 
