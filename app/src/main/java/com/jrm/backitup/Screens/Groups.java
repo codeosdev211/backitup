1912bf6a871e2487dd84134fe9b7766769bca8e0
codeos <codeos.dev211@gmail.com>
2020-11-11 14:52:30 +0530

    commit 11Nov20 1452

--- app/src/main/java/com/jrm/backitup/Screens/Groups.java
+++ app/src/main/java/com/jrm/backitup/Screens/Groups.java
@@ -1,16 +1,74 @@
 package com.jrm.backitup.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
+import com.jrm.backitup.Local.AppPref;
 import com.jrm.backitup.R;
 
+import org.json.JSONObject;
+
+/*
+ *  @Author Jayesh (codeos)
+ * I hate comments but this is for you to understand my strange code.
+ * My function names will mostly explain the code.
+ */
 public class Groups extends AppCompatActivity {
+
+    // currentUser will hold the values we stored in shared preference while signing in
+    JSONObject currentUser = null;
+
+    // layout that contains the search stuff
+    ConstraintLayout searchPanel;
+    Animation openPanel, closePanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.groups);
+        try {
+            currentUser = new JSONObject(new AppPref().localData(getApplicationContext(), 'G', "BU", ""));
+
+            searchPanel = (ConstraintLayout) findViewById(R.id.grpSearchPanel);
+
+            // loading animations
+            openPanel = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.top_down);
+            closePanel = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.top_up);
+
+
+
+        }catch(Exception error) {
+            toast("Could not load page", 1);
+        }
     }
+
+    // defaults
+    private void redirect(Class<?> destination) {
+        Intent goTo = new Intent(getApplicationContext(), destination);
+        startActivity(goTo);
+        finish();
+    }
+    private void toast(String message, int duration) {
+        Toast.makeText(getApplicationContext(), message, duration).show();
+    }
+
+    // onclick
+
+    public void openSearch(View view) {
+         searchPanel.startAnimation(openPanel);
+         searchPanel.setVisibility(View.VISIBLE);
+    }
+
+    public void closeSearch(View view) {
+        searchPanel.startAnimation(closePanel);
+        searchPanel.setVisibility(View.GONE);
+    }
+
 }
